d3ad2693999be810649f0d8187f5c0db30e55659
Paul M Furley <paul@paulfurley.com>
2018-04-17 16:00:47 +0100

    Make flask app "interactive"

--- app.py
+++ app.py
@@ -1,25 +1,29 @@
 #!/usr/bin/env python
 
 import io
+import json
 import datetime
-import sys
-import time
+import struct
 
 from os.path import join as pjoin
+from collections import OrderedDict
 
 import grpc
 import trillian_log_api_pb2
 import trillian_log_api_pb2_grpc
 
 from pathlib import Path
-from flask import Flask
+from flask import Flask, render_template, request
 
 HOME_DIR = str(Path.home())
 
 app = Flask(__name__)
 
-with io.open(pjoin(HOME_DIR, '.log_id'), 'r') as f:
-    log_id = int(f.read())
+LOG_ID_FILENAME = pjoin(HOME_DIR, '.log_id')
+
+with io.open(LOG_ID_FILENAME, 'r') as f:
+    LOG_ID = int(f.read())
+    print('Read log ID {} from {}'.format(LOG_ID, LOG_ID_FILENAME))
 
 
 class Trillian():
@@ -37,17 +41,76 @@ class Trillian():
         )
         return self.__stub.QueueLeaf(request)
 
+    def get_recent_leaves(self, number_of_leaves):
+        tree_size = self.get_tree_size()
 
-TRILLIAN = Trillian('localhost', '8090', log_id)
+        request = trillian_log_api_pb2.GetLeavesByIndexRequest(
+            log_id=self.__log_id,
+        )
+        indexes = range(tree_size - 1, tree_size - 1 - number_of_leaves, -1)
+        request.leaf_index.extend(indexes)
+
+        response = self.__stub.GetLeavesByIndex(request)
+        return response
+
+    def get_tree_size(self):
+        return self.get_signed_log_root()['tree_size']
+
+    def get_signed_log_root(self):
+        request = trillian_log_api_pb2.GetLatestSignedLogRootRequest(
+            log_id=self.__log_id,
+        )
+
+        response = self.__stub.GetLatestSignedLogRoot(request)
+        self._validate_log_root_signature(
+            response.signed_log_root.log_root,
+            response.signed_log_root.log_root_signature
+        )
+
+        log_root_decoded = self._deserialize_log_root(
+            response.signed_log_root.log_root
+        )
+
+        return log_root_decoded
+
+    def _validate_log_root_signature(self, log_root, signature):
+        pass  # TODO
+
+    def _deserialize_log_root(self, log_root):
+        # TODO: unpack the whole structure
+        tree_size, = struct.unpack('>Q', log_root[2:10])  # Q = uint64
+
+        return {
+            'tree_size': tree_size
+        }
+
+
+TRILLIAN = Trillian('localhost', '8090', LOG_ID)
 
 
 @app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/add-log/', methods=['POST'])
 def queue_timestamp_to_trillian():
     now = datetime.datetime.now()
-    timestamp_data = now.isoformat().encode('utf-8')
 
-    print('Adding `{}` to Log'.format(timestamp_data))
+    data = json.dumps(OrderedDict([
+        ('timestamp', now.isoformat()),
+        ('message', request.form['log_leaf']),
+    ])).encode('utf-8')
 
-    response = TRILLIAN.queue_leaf(timestamp_data)
+    print('Adding `{}` to Log'.format(data))
 
-    return '<pre>{}</pre>'.format(response)
+    response = TRILLIAN.queue_leaf(data)
+
+    return render_template('add_log.html', response=response)
+
+
+@app.route('/recent-logs/')
+def view_logs():
+    response = TRILLIAN.get_recent_leaves(3)
+
+    return render_template('add_log.html', response=response)
